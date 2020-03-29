adc4237e68f1b12edc41fb1cf0cb764019a22a79
DavidVargas <david.vargas@decameron.com>
2020-03-28 19:08:46 -0500

    Definiendo rutas con react-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = ({message}) => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
     <div className="container">
-      <h1>{message}</h1>
+      <h1>
+        <Link to={"/"} className="text-light" >
+          {message}
+        </Link>
+      </h1>
     </div>
-    <a href="/products/new" className="btn btn-danger nuevo-post d-block d-md-inline-block">Add Product &#43;</a>
+
+    <Link to={"/products/new"} className="btn btn-danger nuevo-post d-block d-md-inline-block">Add Product &#43;</Link>
   </nav>
 );
 
---
+++ src/components/NewProduct.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NewProduct = () => {
+  return (
+    <h1>From new Product</h1>
+  )
+};
+
+export default NewProduct;
